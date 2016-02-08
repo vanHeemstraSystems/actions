d82802ddd8b79c6468831cab570e9b06b733b169
Willem van Heemstra <willem@vanheemstrasystems.com>
2016-02-08 10:52:35 +0000

    Create AddCharacterActions.js

---
+++ AddCharacterActions.js
@@ -0,0 +1,30 @@
+import alt from '../alt'; /* TO DO: ADJUST PATH TO POINT TO alt.js */
+
+class AddCharacterActions {
+  constructor() {
+    this.generateActions(
+      'addCharacterSuccess',
+      'addCharacterFail',
+      'updateName',
+      'updateGender',
+      'invalidName',
+      'invalidGender'
+    );
+  }
+
+  addCharacter(name, gender) {
+    $.ajax({
+      type: 'POST',
+      url: '/api/characters',
+      data: { name: name, gender: gender }
+    })
+      .done((data) => {
+        this.actions.addCharacterSuccess(data.message);
+      })
+      .fail((jqXhr) => {
+        this.actions.addCharacterFail(jqXhr.responseJSON.message);
+      });
+  }
+}
+
+export default alt.createActions(AddCharacterActions);
