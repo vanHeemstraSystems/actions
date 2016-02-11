597da6af3854fa50ebe754297e0f402e3afec244
Willem van Heemstra <willem@vanheemstrasystems.com>
2016-02-11 21:36:32 +0000

    Update LocationActions.js

--- LocationActions.js
+++ LocationActions.js
@@ -1,17 +1,26 @@
-fetchLocations() {
-  return (dispatch) => {
-    // we dispatch an event here so we can have "loading" state.
-    dispatch();
-    LocationSource.fetch()
-      .then((locations) => {
-        // we can access other actions within our action through `this.actions`
-        this.updateLocations(locations);
-      })
-      .catch((errorMessage) => {
-        this.locationsFailed(errorMessage);
-      });
-    }
+var alt = require('../libraries/alt');
+
+class LocationActions {
+  updateLocations(locations) {
+    return locations;
+  }
+  fetchLocations() {
+    return (dispatch) => {
+      // we dispatch an event here so we can have "loading" state.
+      dispatch();
+      LocationSource.fetch()
+        .then((locations) => {
+          // we can access other actions within our action through `this.actions`
+          this.updateLocations(locations);
+        })
+        .catch((errorMessage) => {
+          this.locationsFailed(errorMessage);
+        });
+      }
+  }
+  locationsFailed(errorMessage) {
+    return errorMessage;
+  }
 }
-locationsFailed(errorMessage) {
-  return errorMessage;
-}
+
+module.exports = alt.createActions(LocationActions);
